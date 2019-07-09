611a1867ca7eac3c8402116819fddfb619b474d3
Sergio Christ <37073572+Serzho@users.noreply.github.com>
2019-07-09 23:35:43 +0300

    added empty window

--- UserInterface.py
+++ UserInterface.py
@@ -1,1 +1,11 @@
+#импорт стандартных библиотек
+from tkinter import * #импорт сатндартной библиотеки GUI
 
+class UserInterface():
+    def __init__(self, learning = False):
+        #присваивание значений атрибутов
+        self.__learning = learning #режим обучение
+        #создание окна
+        self.__window = Tk() #объект окна
+        self.__window.title("Handreader") #создание заголовка окна
+        self.__window.mainloop() #запуск цикла окна
